f62b5d667f8e163a5a7bf4b48850bef849b54e9b
Alec House <staticunit97@gmail.com>
2025-04-10 22:05:37 -0500

    refactor: update ingredient and serving price handling in Recipe class and adjust CSV headers

--- recipe_helper/recipe.py
+++ recipe_helper/recipe.py
@@ -11,32 +11,49 @@ class Recipe:
     def __init__(self, basedir: Path, prices: Prices):
         self.__basedir = basedir
         self.__prices = prices
-        self.each_price: dict[str, Quantity] = {}
+        self.ingredient_price: dict[str, Quantity] = {}
+        self.serving_price: dict[str, Quantity] = {}
         self.price = 0.0 * UREG.dollar
         self.__process_ingredients()
+        self.__process_servings()
 
     def __process_ingredients(self) -> None:
         """Process the ingredients for the recipe"""
 
         fname = self.__basedir / "ingredients.csv"
         df = pd.read_csv(fname, keep_default_na=False, index_col="name")
-        # df = df.T
 
         for name, vals in df.iterrows():
             qty: Quantity = vals["quantity"] * UREG[vals["unit"]]
             price = self.__prices.compute_price(name, qty)
-            self.each_price[name] = price
+            self.ingredient_price[name] = price
             self.price += price
 
-        self._df = df
+        self._ingredients = df
 
-    def __repr__(self) -> str:
-        out = []
-        out += [f"Recipe: {self.__basedir.name}"]
-        out += ["Ingredients:"]
-        for n, p in self.each_price.items():
-            s = f"    - {n}: {self._df.at[n, 'quantity']} {self._df.at[n, 'unit']}"
-            s += f" -> ${p.magnitude:0.2f}"
-            out += [s]
-        out += [f"Total Price: ${self.price.magnitude:0.2f}"]
-        return "\n".join(out)
+    def __process_servings(self) -> None:
+        """Process the servings for the recipe"""
+        fname = self.__basedir / "servings.csv"
+        df = pd.read_csv(fname, keep_default_na=False, index_col="form")
+
+        # Check that no servings are zero
+        if df["servings"].eq(0).any():
+            raise ValueError(f"Error when reading {fname}, number of servings cannot be zero.")
+
+        # Calculate the price per serving
+        for form, row in df.iterrows():
+            servings = row["servings"]
+            self.serving_price[form] = self.price.magnitude / servings
+
+        self._servings = df
+
+    # def __repr__(self) -> str:
+    #     out = []
+    #     out += [f"Recipe: {self.__basedir.name}"]
+    #     out += ["Ingredients:"]
+    #     for n, p in self.ingredient_price.items():
+    #         s = f"    - {n}: {self._ingredients.at[n, 'quantity']} {self._ingredients.at[n, 'unit']}"
+    #         s += f" -> ${p.magnitude:0.2f}"
+    #         out += [s]
+    #     out += [f"Total Price: ${self.price.magnitude:0.2f}"]
+    #     return "\n".join(out)
